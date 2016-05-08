ff4ecc02c10d830f6283931db3662777511b86ab
Liu Lantao <liulantao@gmail.com>
2016-05-08 12:14:13 +0800

    NSURLSession uses proxy config

--- app/helpers/RMURLProtocol.rb
+++ app/helpers/RMURLProtocol.rb
@@ -1,8 +1,16 @@
 class RMURLProtocol < NSURLProtocol
+  @@protocolKey ||= 'RMURLProtocolKey'
+  @@requestCount ||= 0
+
   def self.canInitWithRequest(request)
-    @@requestCount ||= 0
     NSLog "Request #%u: URL = %@" % [@@requestCount+=1, request.URL.absoluteString]
-    return false
+
+    if NSURLProtocol.propertyForKey @@protocolKey, inRequest:request
+      return false
+    end
+
+    scheme = request.URL.scheme.lowercaseString
+    return scheme.isEqualToString("http") || scheme.isEqualToString("https")
   end
 
   def self.canonicalRequestForRequest(request)
@@ -14,7 +22,22 @@ class RMURLProtocol < NSURLProtocol
   end
 
   def startLoading
-    @connection = NSURLConnection.connectionWithRequest(self.request, delegate: self)
+    request = self.request.mutableCopy
+    NSURLProtocol.setProperty true, forKey:@@protocolKey, inRequest:request
+    config = NSURLSessionConfiguration.defaultSessionConfiguration
+    config.requestCachePolicy = NSURLRequestReloadIgnoringLocalCacheData
+    config.connectionProxyDictionary = {
+      KCFNetworkProxiesHTTPEnable => 1,
+      KCFNetworkProxiesHTTPProxy => "42.120.23.151",
+      KCFNetworkProxiesHTTPPort => 13128,
+      'HTTPSEnable' => 1,
+      KCFStreamPropertyHTTPSProxyHost => "42.120.23.151",
+      KCFStreamPropertyHTTPSProxyPort => 13128
+    }
+    m_session = NSURLSession.sessionWithConfiguration config, delegate:self, delegateQueue:NSOperationQueue.currentQueue
+    m_session.dataTaskWithRequest(request).resume
+
+    # @connection = NSURLConnection.connectionWithRequest(request, delegate: self)
   end
 
   def stopLoading
